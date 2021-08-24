5e0a0a743156b0ea73ba7540078fc7a0dadece1b
ilhankalac <ilhan.kalac@codeus.me>
2021-08-24 23:54:00 +0200

    leaflet library 0.9

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <l-map style="height: 300px" :zoom="zoom" :center="center">
+  <l-map style="height: 1000px; width:100%" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
     <l-marker :lat-lng="markerLatLng"></l-marker>
   </l-map>
@@ -24,26 +24,48 @@ export default {
             url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
             attribution:
                 '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-            zoom: 15,
+            zoom: 13,
             center: [51.505, -0.159],
             markerLatLng: [51.504, -0.159],
+            location: null,
+            gettingLocation: false,
+            errorStr: null,
         };
     },
-    created() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                const { latitude } = position.coords;
-                const { longitude } = position.coords;
-
-                console.log(latitude);
-                console.log(longitude);
-            });
-        }
-    },
     methods: {
-        test() {
-            this.center = [42.8415336, 20.176009099999998];
+        async getLocation() {
+            return new Promise((resolve, reject) => {
+                if (!("geolocation" in navigator)) {
+                    reject(new Error("Geolocation is not available."));
+                }
+                navigator.geolocation.getCurrentPosition(
+                    (pos) => {
+                        resolve(pos);
+                    },
+                    (err) => {
+                        reject(err);
+                    }
+                );
+            });
         },
+    },
+    async created() {
+        this.gettingLocation = true;
+        try {
+            this.gettingLocation = false;
+            this.location = await this.getLocation();
+            this.center = [
+                this.location.coords.latitude,
+                this.location.coords.longitude,
+            ];
+            this.markerLatLng = [
+                this.location.coords.latitude,
+                this.location.coords.longitude,
+            ];
+        } catch (e) {
+            this.gettingLocation = false;
+            this.errorStr = e.message;
+        }
     },
 };
 </script>
