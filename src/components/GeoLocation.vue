805cb6e1c7c1eedfa82c909cede4905c3065a549
ilhankalac <ilhan.kalac@codeus.me>
2021-08-25 19:39:07 +0200

    renaming component to geolocation

--- src/components/GeoLocation.vue
+++ src/components/GeoLocation.vue
@@ -14,6 +14,7 @@ import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
 import "leaflet/dist/leaflet.css";
 
 export default {
+    name: "GeoLocation",
     components: {
         LMap,
         LTileLayer,
