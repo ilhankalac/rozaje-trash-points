461c76760f1206f90d456301cbaf07422e7a8b7a
ilhankalac <ilhan.kalac@codeus.me>
2022-01-25 14:25:55 +0100

    backgroudn color property fix

--- src/App.vue
+++ src/App.vue
@@ -41,8 +41,7 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    
+    background-color: #2c3e50;
 }
 .topnav {
   background-color: #486684;
