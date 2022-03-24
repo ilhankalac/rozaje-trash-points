30b7e854fba3be9adc4ebb60b93dd90d5cb1c376
ilhankalac <ilhan.kalac@codeus.me>
2022-03-24 10:43:22 +0100

    Adding vuex store to project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import firebase from './firebase/firebase'
 import { rtdbPlugin } from 'vuefire'
+import { store } from './store.js'
 
 Vue.use(rtdbPlugin)
 Vue.config.productionTip = false
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   vuetify,
   firebase,
+  store,
   render: h => h(App)
 }).$mount('#app')
