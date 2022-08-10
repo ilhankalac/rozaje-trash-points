57523c9eb767e8e275e3a7cbd445bb061fd8ecd1
ilhankalac <ilhan.kalac@codeus.me>
2022-08-10 10:32:42 +0200

    configuring project with prod service

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,7 @@
 import firebase from 'firebase'
+
+
+// DEV
 var firebaseConfig = {
   apiKey: "AIzaSyDhf77MARLLZfNv90SsmDLGAuF7Ru7QraQ",
   authDomain: "rozaje-trash-points.firebaseapp.com",
@@ -9,6 +12,20 @@ var firebaseConfig = {
   appId: "1:228048810122:web:0871fc0c4445c3bda3fd73",
   measurementId: "G-M6SP15V8F5"
 };
+
+
+// PROD
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCxR49ADcElI1SMPnRWyhHBQM14iXuorjY",
+//   authDomain: "prod-rozaje-trash-points.firebaseapp.com",
+//   databaseURL: "https://prod-rozaje-trash-points-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "prod-rozaje-trash-points",
+//   storageBucket: "prod-rozaje-trash-points.appspot.com",
+//   messagingSenderId: "434166427086",
+//   appId: "1:434166427086:web:076cc4c41e1dfa18dccf9f",
+//   measurementId: "G-X6CHVEDZX6"
+// };
+
 const app = firebase.initializeApp(firebaseConfig);
 export default firebase;
 export const db = app.database();
